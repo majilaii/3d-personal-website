b76d8afc3dd46fedc255d9e86927dcabf90cdd51
Kuong Ao Ieong <majilaiwife@gmail.com>
2023-04-17 19:37:40 +0100

    initial commit

---
+++ src/components/About.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+import { styles } from "../style";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../HOC";
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        A full-stack software engineer with a background in economics and
+        mathematics, I have a passion for analyzing and creating solutions using
+        different technologies - Node, Express, Koa, Mongo, SQL, WebSockets,
+        React, React-Native, and Angular. I'm a self-starter who's passionate
+        about problem-solving and always looking for ways to improve and push
+        myself.
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => {
+          return <ServiceCard key={service.title} index={index} {...service} />;
+        })}
+      </div>
+    </>
+  );
+};
+
+function ServiceCard({ index, title, icon }) {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full  green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450,
+          }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col "
+        >
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-old text-center">
+            {title}
+          </h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+}
+
+export default SectionWrapper(About, "about");
